c80d331f549248b57c97e96ad7f5be22b4cb813a
Lily Yassemi <lay6@humboldt.edu>
2025-04-21 06:27:11 -0700

    implemented basic google Calendar

--- humboldtgrange/src/pages/Calendar.jsx
+++ humboldtgrange/src/pages/Calendar.jsx
@@ -4,7 +4,31 @@ export function Calendar () {
     return (
         <>
             <Helmet><title>Calendar</title></Helmet>
-            <h1>Calendar</h1>
+            <h1>Calendar of Events</h1>
+
+            <div>
+                {/* Full View (hidden on small screens) */}
+                <iframe
+                    className="calendar-full"
+                    src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=UTC&showPrint=0&title=Humboldt%20Grange%20%23501%20Events&src=MmIwMTI0ZTk0OTVjN2NhZTg4N2VlNWJjNDllYmU3NmJmNDkxNDYwZmJkZjQwYTdhMmUzYTZhMGNiY2Y3NzMzZUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23009688&mode=WEEK"
+                    style={{ border: 'none', width: '100%', height: '600px' }}
+                    title="Humboldt Grange #501 Events"
+                    frameBorder="0"
+                    scrolling="no"
+                />
+                
+                {/* Agenda View (visible only on small screens) */}
+                    <div className="calendar-agenda-div">
+                    <iframe
+                        className="calendar-agenda"
+                        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=UTC&showPrint=0&title=Humboldt%20Grange%20%23501%20Events&src=MmIwMTI0ZTk0OTVjN2NhZTg4N2VlNWJjNDllYmU3NmJmNDkxNDYwZmJkZjQwYTdhMmUzYTZhMGNiY2Y3NzMzZUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23009688&mode=AGENDA"
+                        style={{ border: 'none', width: '100%', height: '600px' }}
+                        title="Humboldt Grange #501 Events"
+                        frameBorder="0"
+                        scrolling="no"
+                    />
+                    </div>
+                </div>
         </>
     )
 }
--- humboldtgrange/src/pages/Home.jsx
+++ humboldtgrange/src/pages/Home.jsx
@@ -18,20 +18,22 @@ export function Home () {
                 <p className="p1">The Grange is a family-friendly, community-centered organization with deep roots in America's
                     agricultural heritage. Since 1867, we've brought people together to learn, lead, and serve—empowering 
                     members to make a real difference in their communities through service, education, and grassroots action.
-                </p>
-                <p className="p1">
+                <br/>
+                <br/>
                     Whether you're passionate about sustainable agriculture, interested in community service, or looking for a 
                     place for your family to connect and grow—there's a place for you at the Humboldt Grange #501.
                 </p>
-
-                <h2>Join Our Community</h2>
-                <p className="p2"> Grange membership is affordable, family-friendly, and full of benefits for everyone. 
-                    Active members enjoy <strong>discounted rental rates</strong> for our Grange Hall, 
-                    access <span><a href="https://www.nationalgrange.org/our-values/benefits-to-members/" target="_blank" rel="noreferrer">National Grange benefits</a></span>, 
-                    leadership and civic engagement opportunities, and the chance to participate in social events, service projects, 
-                    and lifelong learning.
-                </p>
-                <PrimaryButton to="/membership">Become a Member Today</PrimaryButton>
+                <div className="homepage-welcome-text-subsection">
+                    <h2>Help Us Grow Our Community</h2>
+                    <p className="p2"> Grange membership is affordable, family-friendly, and full of benefits for everyone. 
+                        Active members enjoy <strong>discounted rental rates</strong> for our Grange Hall, 
+                        access <span><a href="https://www.nationalgrange.org/our-values/benefits-to-members/" target="_blank" rel="noreferrer">National Grange benefits</a></span>, 
+                        leadership and civic engagement opportunities, and the chance to participate in social events, service projects, 
+                        and lifelong learning.
+                    </p>
+                    <PrimaryButton to="/membership">Become a Member Today</PrimaryButton>
+                </div>
+                
             </div>
             <div className="subscribe-form-div">
                 <SubscribeForm />
