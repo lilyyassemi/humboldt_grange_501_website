57545ae04c544915563c5abae3f0a53ddcad56a0
Lily Yassemi <lay6@humboldt.edu>
2025-04-22 14:28:36 -0700

    created about us page

--- humboldtgrange/src/pages/About.jsx
+++ humboldtgrange/src/pages/About.jsx
@@ -1,6 +1,7 @@
 
 import { Helmet } from 'react-helmet';
 import { useEffect } from 'react';
+import ThirdButton from '../components/ThirdButton';
 
 export function About () {
     useEffect(() => {
@@ -10,8 +11,79 @@ export function About () {
     return (
         <>
         <Helmet><title>About Us</title></Helmet>
-
+        <div className="about-container">
             <h1>About Us</h1>
+            <h2>What is the Grange?</h2>
+            <p className="p1">
+                Established in 1867, the Grange is the oldest farm organization in the United States. It is a national fraternal organization dedicated to meeting the needs of rural and agricultural communities through grassroots service, advocacy, and leadership training.
+            </p>
+            <h2>Who can Join?</h2>
+            <p className="p1">
+                The Community/Subordinate Grange is open to anyone of good moral character age 14 or older 
+                who cares about their community and the food system. 
+                We have a Junior Grange program for children age 5 to 14, 
+                that has helped the youngest members of the family learn about community values 
+                and citizenship for more than 100 years. 
+            </p>
+            <p className="p2"><em>Did you know that women have been equal members since the inception of the Grange?</em></p>
+            <ThirdButton to="/membership" >Learn More About Becoming a Member</ThirdButton>
+            <blockquote className="quote">
+                "In essentials, unity; in non-essentials, liberty; in all things, charity." &mdash; Grange Motto
+            </blockquote>
+            <h2>AGRICULTURAL   •   COMMUNITY   •   FAMILY   •   FRATERNAL</h2>
+{/*}
+
+            <div className="acff-div">
+                <h2>AGRICULTURAL</h2>
+                <h2 className="middot">&middot;</h2>
+                <h2>COMMUNITY</h2>
+                <h2 className="middot">&middot;</h2>
+                <h2>FAMILY</h2>
+                <h2 className="middot">&middot;</h2>
+                <h2>FRATERNAL</h2>
+            </div>*/}
+            <div className="acff-info">
+                <div className="org-list-div">
+                    <h3>As an <span className="org-title">Agricultural Organization</span> we:</h3>
+                    <ul>
+                        <li><p>Provide a voice for farmers and rural Americans.</p></li>
+                        <li><p>Influence state and national policies.</p></li>
+                        <li><p>Support economic development in rural areas.</p></li>
+                    </ul>
+                </div>
+
+                <div className="org-list-div">
+                    <h3>As a <span className="org-title">Community Organization</span> we:</h3>
+                    <ul>
+                        <li><p>Lead local service projects.</p></li>
+                        <li><p>Partner with schools, churches, and agencies.</p></li>
+                        <li><p>Promote lifelong learning.</p></li>
+                    </ul>
+                </div>
+
+                <div className="org-list-div">
+                    <h3>As a <span className="org-title">Family Organization</span> we:</h3>
+                    <ul>
+                        <li><p>Welcome members of all ages.</p></li>
+                        <li><p>Offer full membership beginning at age 14.</p></li>
+                        <li><p>Invite children aged 5 to 14 to join our  {' '}
+                            <a href="/">Junior Grange</a> program.</p></li>
+                    </ul>
+                </div>
+
+                <div className="org-list-div">
+                    <h3>As a <span className="org-title">Fraternal Organization</span> we:</h3>
+                    <ul>
+                        <li><p>Promote patriotic and civic activities.</p></li>
+                        <li><p>Provide social and recreational fellowship.</p></li>
+                        <li><p>Foster brotherhood in an atmosphere of mutual concern.</p></li>
+                    </ul>
+                </div>
+
+            </div>
+            
+            
+        </div>
         </>
     )
 }
